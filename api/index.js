528d08ed94d43b90d4eab1f35c57a48aa7fd73db
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-06 13:26:33 +0100

    made the frontend routes, backend routes, contollers, and models

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!!");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
