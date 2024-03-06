49713af31b49e6156e7e704ec95d2abeb1ff106b
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-06 14:29:28 +0100

    Created the sign up page and changed the index.css background color

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,53 @@
+import { Link } from "react-router-dom";
+
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">
+        Create an account!
+      </h1>
+      <form
+        // onSubmit={handleSubmit}
+        className="flex flex-col gap-4"
+      >
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          // onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          // onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          // onChange={handleChange}
+        />
+
+        <button
+          // disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {/* {loading ? 'Loading...' : 'Sign Up'} */}
+          Create an account
+        </button>
+        {/* <OAuth/> */}
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+    </div>
+  );
 };
