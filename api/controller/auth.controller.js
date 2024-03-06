94f2f7089d6bcad004c518d874c982ac09451b58
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-06 15:30:20 +0100

    installed jwt, created signin API route, the access token method and the access token Cookie

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandlerFunc } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +11,28 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created !");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const CheckUser = await User.findOne({ email: email });
+    if (!CheckUser) return next(errorHandlerFunc(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, CheckUser.password);
+    if (!validPassword)
+      return next(errorHandlerFunc(401, "Incorrect login information!"));
+    const token = jwt.sign({ id: CheckUser._id }, process.env.JWT_SECRET); //created the token
+
+    const { password: hashedPassword, ...rest } = CheckUser._doc; // returning everything except the password
+
+    // save the access token as Cookie
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
