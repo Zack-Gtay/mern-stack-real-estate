9b3a7072fa17177dca10d1ee6e5750b8c4f85079
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-06 13:55:22 +0100

    ADDED SIGN UP API

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created !");
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created !");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
