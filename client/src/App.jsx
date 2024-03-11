10e0c3804e3a007f205ecd5e97135234b58364ae
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-11 22:35:58 +0000

    added create-listing, update-listing routes and protected them, also added the public listing/:listingId route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import About from "./pages/About";
 import { Profile } from "./pages/Profile";
 import { Header } from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import { UpdateListing } from "./pages/UpdateListing";
+import { Listing } from "./pages/Listing";
 
 function App() {
   return (
@@ -16,8 +19,14 @@ function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
