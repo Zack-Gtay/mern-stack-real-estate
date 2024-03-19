d171c4ae501cf8a4ba3d183369af82556148da12
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-19 20:44:28 +0000

    created fetchOfferListings, fetchRentListings, fetchSaleListings, Swiper in Home page,

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -28,7 +28,7 @@ const ListingItem = ({ listing }) => {
             {listing.description}
           </p>
           <p className="text-slate-500 mt-2 font-semibold ">
-            $
+            MAD<span> </span>
             {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
