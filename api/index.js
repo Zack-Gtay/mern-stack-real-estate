db022dac83648192fd20f60990ed2a719a5a4589
zack-gtay <zakariaelgtay2@gmail.com>
2024-03-29 19:20:27 +0000

    added path

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
